e096d365e0ce72f5643614a1dd9e1c47385f5d24
ArjunSahlot <iarjun.sahlot@gmail.com>
2020-12-23 14:03:40 -0800

    constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+import os
+
+WIDTH, HEIGHT = 1000, 1000
+
+FPS = 90
+
+# Colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+
+PARDIR = os.path.realpath(os.path.dirname(__file__))
