bce42efb30d1d8876ce80d532756fbb38db143bf
ArjunSahlot <iarjun.sahlot@gmail.com>
2020-12-25 22:29:19 -0800

    draw fourier

--- interface.py
+++ interface.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import *
-from cmath import exp, pi, phase as atan2, sqrt
+from cmath import exp, pi, phase as atan2, sqrt, cos, sin
 
 
 class Interface:
     def __init__(self, x, y, width, height, mode):
         self.x, self.y, self.width, self.height = x, y, width, height
         self.mode = mode
+        self.cycles = []
+        self.time = 0
     
     def update(self, window, events):
         self.draw(window)
@@ -14,12 +16,29 @@ class Interface:
     def draw(self, window):
         pass
 
+    def draw_fourier(self, window):
+        x, y = self.x + self.width/2, self.y + self.height/2
+
+        for cycle in self.cycles:
+            prev_x = x
+            prev_y = y
+            freq = cycle["freq"]
+            radius = cycle["amp"]
+            phase = cycle["phase"]
+            x += radius * cos(freq * self.time + phase)
+            y += radius * sin(freq * self.time + phase)
+
+            pygame.draw.circle(window, RED if len(cycle) == 4 else WHITE, (prev_x.real, prev_y.real), radius, 1)
+            pygame.draw.line(window, RED if len(cycle) == 4 else WHITE, (prev_x.real, prev_y.real), (x.real, y.real), 3)
+
+        return (x.real, y.real)
+
     def dft(self, _points, sort, reverse):
         points = []
         for x, y in _points:
             points.append(complex(x, y))
 
-        cycles = []
+        self.cycles = []
         N = len(points)
         for k in range(N):
             summation = complex(0, 0)
@@ -32,15 +51,15 @@ class Interface:
             freq = k
             phase = atan2(summation)
             amp = sqrt(summation.real**2 + summation.imag**2).real
-            cycles.append({"freq": freq, "phase": phase, "amp": amp})
+            self.cycles.append({"freq": freq, "phase": phase, "amp": amp})
 
         if sort.lower() == "radius":
-            cycles.sort(key=lambda x: x["amp"], reverse=reverse)
+            self.cycles.sort(key=lambda x: x["amp"], reverse=reverse)
         elif sort.lower() == "phase":
-            cycles.sort(key=lambda x: x["phase"], reverse=reverse)
+            self.cycles.sort(key=lambda x: x["phase"], reverse=reverse)
         elif sort.lower() == "speed":
-            cycles.sort(key=lambda x: x["freq"], reverse=reverse)
+            self.cycles.sort(key=lambda x: x["freq"], reverse=reverse)
 
-        cycles[-1]["last"] = True
+        self.cycles[-1]["last"] = True
 
-        return cycles
+        return self.cycles
